1399e922598c5ccf2c32ad6645191047671b1a66
bwall <brendan.wall03@gmail.com>
2020-06-30 16:58:26 +0100

    Try to add image on hover

--- Script.js
+++ Script.js
@@ -5,6 +5,18 @@ testApp.controller("testController", function($scope, $http) {
     $scope.driversList = [];
     $scope.constructorsList = [];
     $scope.years = [2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010];
+
+    $scope.displayImage = false;
+
+    $scope.showImage = function(imageUrl) {
+        $scope.currentImageUrl = "images/mercedes.jpg";
+        $scope.displayImage = true;
+    }
+
+    $scope.hideImage = function(imageUrl) {
+        $scope.displayImage = false;
+    }
+
     //    Our GET request function
     $scope.getRequest = function() {
         console.log("I've been pressed!");
